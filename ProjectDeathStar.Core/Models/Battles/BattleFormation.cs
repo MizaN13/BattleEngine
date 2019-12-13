3906a50a7f33304d5b642ffc66918ea2eea477f0
mizan <mizan.441980@gmail.com>
2019-12-14 02:28:07 +0600

    Upated version of DeathStar(Test Cases and layering added)

---
+++ ProjectDeathStar.Core/Models/Battles/BattleFormation.cs
@@ -0,0 +1,147 @@
+﻿using DeathStar.Core.Models.Ships;
+using System;
+
+namespace DeathStar.Core.Models.Battles
+{
+    public class BattleFormation
+    {
+        public string Name { get; private set; }
+        public FormationRow[] Rows { get; private set; }
+        public bool IsActive { get; private set; }
+        
+
+        public General GeneralInCharge { get; private set; }
+
+        public BattleFormation(string name)
+        {
+            Name = name;
+            Rows = new FormationRow[6];
+        }
+
+        public void SetStatus(bool isActive)
+        {
+            IsActive = isActive;
+        }
+
+        /* isp 
+         
+         Input variables: 
+            SelectedShip = valid ship names, invalid ship names 
+            amountOfShips = 0, >0
+            rowposition = <0, >0 && <6, >=6
+
+         State variables:
+            row = empty || null, not empty
+            rowPosition = null, ship formation exists
+            
+        */ 
+        public void SetFormationRow(Ship selectedShip, uint amountOfShips, uint rowPosition)
+        {
+            if (Rows[rowPosition] != null)
+            {
+                throw new Exception("Slot not empty");
+            }
+            else if (rowPosition < 6 && rowPosition >=0)
+            {
+                Rows[rowPosition] = new FormationRow(selectedShip, amountOfShips);
+            }
+            else
+            {
+                throw new Exception("Invalid Row Position");
+            }
+        }
+
+        /* isp 
+         
+         Input variables: 
+            SelectedShip = valid ship names, invalid ship names 
+            amountOfShips = 0, >0
+            rowposition = <0, >0 && <6, >=6
+
+         State variables:
+            row = empty || null, not empty
+            rowPosition = null, ship formation exists
+            
+        */
+
+        public void UpdateFormationRow(Ship selectedShip, uint amountOfShips, uint rowPosition) {
+            if (rowPosition < 6 && rowPosition >= 0)
+            {
+                Rows[rowPosition] = new FormationRow(selectedShip, amountOfShips);
+            }
+            else
+            {
+                throw new Exception("Invalid Row Position");
+            }
+        }
+
+        /* isp 
+         
+         Input variables: 
+            rowposition = <0, >0 && <6, >=6
+
+         State variables:
+            row = empty || null, not empty
+            rowPosition = null, ship formation exists
+            
+        */
+        public void RemoveFormationRow(uint rowPosition) {
+            if (rowPosition < 6 && rowPosition >= 0)
+            {
+                Rows[rowPosition] = null;
+            }
+            else
+            {
+                throw new Exception("Invalid Row Position");
+            }
+        }
+
+        /* isp 
+         
+         Input variables: 
+            general = null, String
+
+         State variables:
+            GeneralInCharge = null, not null
+            
+        */
+
+        public void SetGeneral(General general)
+        {
+            if (general is null)
+                throw new ArgumentNullException(nameof(general));
+            else if(GeneralInCharge != null)
+                throw new Exception("General already exists");
+            GeneralInCharge = general;
+        }
+
+        /* isp 
+         
+         Input variables: 
+            general = null, String
+
+         State variables:
+            GeneralInCharge = null, not null
+            
+        */
+
+        public void UpdateGeneral(General general)
+        {
+            if (general is null)
+                throw new ArgumentNullException(nameof(general));
+            GeneralInCharge = general;
+        }
+
+        /* isp 
+         
+         Input variables: 
+            isActive = true, false
+
+         State variables:
+            IsActive = true, false
+            
+        */
+        public void SetFormationType(bool isActive = false)
+            => IsActive = isActive;
+    }
+}
---
+++ ProjectDeathStar.Core/Models/Equipments/DefenseModule.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace DeathStar.Core.Models.Equipments
+{
+	public class DefenseModule
+	{
+		public virtual string Name { get; private set; }
+		public virtual int ShieldEffectFactor { get; private set; }
+		public virtual int ArmorEffectFactor { get; private set; }
+		public virtual int HullEffectFactor { get; private set; }
+
+		public virtual DefenseModuleGrades Size { get; private set; }
+		public virtual uint Bonus { get; private set; }
+
+		public DefenseModule(DefenseModules defenseModules, DefenseModuleGrades defenseModuleGrades)
+		{
+			Name = defenseModules.ToString();
+			Size = defenseModuleGrades;
+
+			if (defenseModules == DefenseModules.ShieldAmplifier)
+			{
+				switch (defenseModuleGrades)
+				{
+					case DefenseModuleGrades.Medium:
+						Bonus = 20;
+						break;
+					case DefenseModuleGrades.Large:
+						Bonus = 50;
+						break;
+					case DefenseModuleGrades.XLarge:
+						Bonus = 100;
+						break;
+					case DefenseModuleGrades.XXLagre:
+						Bonus = 500;
+						break;
+					case DefenseModuleGrades.Small:
+						Bonus = 5;
+						break;
+                    case DefenseModuleGrades.Doomsday:
+                        Bonus = 0;
+                        break;
+                    default:
+						throw new InvalidOperationException("Invalid Size");
+				}
+			}
+			else if (defenseModules == DefenseModules.ArmorPlate)
+			{
+				Bonus = 0;
+			}
+			else if (defenseModules == DefenseModules.HullPlate)
+			{
+				switch (Size)
+				{
+					case DefenseModuleGrades.Small:
+						Bonus = 10;
+						break;
+					case DefenseModuleGrades.Medium:
+						Bonus = 20;
+						break;
+					case DefenseModuleGrades.Large:
+						Bonus = 50;
+						break;
+					case DefenseModuleGrades.XLarge:
+						Bonus = 100;
+						break;
+					case DefenseModuleGrades.XXLagre:
+						Bonus = 500;
+						break;
+                    case DefenseModuleGrades.Doomsday:
+                        Bonus = 0;
+                        break;
+                    default:
+						throw new InvalidOperationException("Invalid size");
+				}
+			}
+			else
+				throw new InvalidOperationException("Invalid defensive module");
+		}
+
+	}
+
+
+}
+
